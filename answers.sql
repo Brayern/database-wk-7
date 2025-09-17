115b6d6137f5c59b191121e31df2fef9685da5ce
Brayern <34036871+Brayern@users.noreply.github.com>
2025-09-17 16:30:33 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -24,4 +24,31 @@ INSERT INTO OrderProducts (OrderID, Product) VALUES
 (102, 'Mouse'),
 (103, 'Phone');
 
---Question 2
+--Question 2, 2NF
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE OrderProducts (
+    OrderProductID INT PRIMARY KEY AUTO_INCREMENT,
+    OrderID INT,
+    Product VARCHAR(100) NOT NULL,
+    Quantity INT NOT NULL,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO Orders (OrderID, CustomerName) VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+INSERT INTO OrderProducts (OrderID, Product, Quantity) VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
+
+
